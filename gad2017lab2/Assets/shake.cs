98c155acc2d8d6282d4c03eb39a7bd359db79585
Batuhan <batuhancanatar@gmail.com>
2018-10-05 08:25:19 +0000

    efe hoca

---
+++ gad2017lab2/Assets/shake.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class shake : MonoBehaviour {
+
+	public float maxSpeed = 3;
+	bool facingRight;
+	public float velocity=3f;
+
+	void FixedUpdate () 
+	{
+		float move = Input.GetAxis ("Horizontal");
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
